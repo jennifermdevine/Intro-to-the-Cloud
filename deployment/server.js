bf9f0b668bd5fcc5897b168a35cdde440c6734ee
Jennifer Devine <jennifermdevine88@gmail.com>
2023-07-16 11:54:55 -0700

    getting started w/first steps

---
+++ deployment/server.js
@@ -0,0 +1,22 @@
+//dependencies
+
+const express = require('express')
+const app = express()
+
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+//routes
+
+app.get('/', (req, res) =>{
+    res.send('Hello, world!')
+})
+
+
+
+
+//listen
+
+app.listen(3000, function() { 
+    console.log(`Server is running! http://localhost:${PORT}`)
+})
